ac911b89fe27c2c37a12adc6deaf4d68b5857d7d
Shabbirsyed05 <119849465+Shabbirsyed05@users.noreply.github.com>
2025-01-01 18:16:29 +0530

    Update backend.tf

--- ToDo-App/backend.tf
+++ ToDo-App/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "10weeksofcloudops-week4"
+    bucket = "bucket-new"
     key    = "backend/ToDo-App.tfstate"
     region = "us-east-1"
     dynamodb_table = "10weeksofcloudops-dynamo"
